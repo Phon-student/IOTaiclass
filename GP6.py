cb69d932d04147d84a4e6a9fce4389a53d9269bb
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2025-02-14 10:00:44 +0700

    Fix message handling and connection logic in MQTT client

--- GP6.py
+++ GP6.py
@@ -4,16 +4,16 @@ import RPi.GPIO as GPIO
 broker = "iot.kmitlnext.com"
 port = 9001
 
-def on_message(client, userdata, message):
+def on_message(client, _userdata, message):
     print(f"Message received: {message.payload.decode()} + \n")
-    if (message.payload.decode() == 0):
+    if (message.payload.decode() == "0"):
         client.publish("TanakornHome/LampSta" , "0")
         GPIO.output(4, False)
     else:
         client.publish("TanakornHome/LampSta" , "1")
         GPIO.output(4, True)
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(_client, _userdata, _flags, rc):
     if rc == 0:
         print("Connected to broker")
     else:
@@ -24,16 +24,16 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 
-client = mqtt.Client(transprot="websockets")
+client = mqtt.Client(transport="websockets")
 client.username_pw_set(username="kmitliot", password="KMITL@iot1234")
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(broker, port)
 client.subscribe("TanakornHome/LampCmd")
-client.loop_forever()
-# except KeyboardInterrupt:
-if KeyboardInterrupt:
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
     GPIO.cleanup()
     client.disconnect()
     print("Exit")
